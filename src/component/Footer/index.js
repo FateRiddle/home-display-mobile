d3008a6a303586e02c420be78fc9e6c71ec67efb
FateRiddle <Riddle@luosangs-MacBook-Air.local>
2017-03-09 11:51:39 +0800

    收藏图标

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import shoucang from '../../image/icons/shoucang.png'
-import gouwuche from '../../image/icons/gouwuche.png'
+import shoucangUrl from '../../image/icons/shoucang.png'
+import shoucang2Url from '../../image/icons/shoucang2.png'
+import gouwucheUrl from '../../image/icons/gouwuche.png'
 
 class Footer extends React.Component {
 
+  state = {shoucang:false}
+
   render() {
+    const { shoucang } = this.state
     return <div className="Footer">
-      <section>
-        <img src={shoucang} alt=""/>
+      <section onClick={()=>this.setState({shoucang:!shoucang})}>
+      {
+        shoucang?<img src={shoucang2Url} alt=""/>:<img src={shoucangUrl} alt=""/>
+      }
+
         <figure>收藏</figure>
       </section>
       <div className="Footer-line"></div>
       <section>
-        <img src={gouwuche} alt=""/>
+        <img src={gouwucheUrl} alt=""/>
         <figure>购物车</figure>
       </section>
       <button>加入购物车</button>
