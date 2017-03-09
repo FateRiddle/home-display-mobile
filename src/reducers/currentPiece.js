e271bc6965e6faeadda038e6944f25ed49219f76
FateRiddle <Riddle@luosangs-MacBook-Air.local>
2017-03-09 14:04:18 +0800

    兼容性，不用Array.from

--- src/reducers/currentPiece.js
+++ src/reducers/currentPiece.js
@@ -2,7 +2,8 @@ const currentPiece = (state = ['0','0','0','0','0'] , action) => {
   //state一开始无,全用0表示好用于判断，正常的会是类似['11','21','31','41','51']
   switch (action.type) {
     case "CHANGE_PIECE":
-      const [ tabId, _ ] = Array.from(action.id) //从string里取第一个字符
+      const tabId = action.id.substring(0,1)
+      // const [ tabId, _ ] = Array.from(action.id)
       if (['1','2','3','4','5'].indexOf(tabId) > -1) {
         return [
           ...state.slice(0, tabId-1),
